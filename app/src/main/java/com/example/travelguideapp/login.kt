b99ebf8786539c7a8ab9db700697b8009cfdf697
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-04-08 19:25:33 +0530

    Login and signup with Id/password

--- app/src/main/java/com/example/travelguideapp/login.kt
+++ app/src/main/java/com/example/travelguideapp/login.kt
@@ -3,95 +3,36 @@ package com.example.travelguideapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.inputmethod.InputBinding
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.auth.FirebaseAuth
-import com.example.travelguideapp.databinding.ActivityMainBinding
-import com.google.android.gms.auth.GoogleAuthException
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class login : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var emailid: TextInputEditText
-    private lateinit var password: TextInputEditText
-    private lateinit var login1: CardView
-    private lateinit var register: TextView
-    private lateinit var login_google: CardView
-    lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-
-    private companion object{
-        private const val RC_SIGN_IN = 100
-        private const val TAG = "Google_sign_in_tag"
-    }
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
 
-        emailid = findViewById(R.id.emailid)
-        password = findViewById(R.id.password)
-        login1 = findViewById(R.id.login1)
-        register = findViewById(R.id.signup)
-        login_google = findViewById(R.id.loginwithgoogle)
-        auth = FirebaseAuth.getInstance()
+        var emailid = findViewById<TextInputEditText>(R.id.emailid)
+        var password = findViewById<TextInputEditText>(R.id.password)
+        var login1 = findViewById<CardView>(R.id.login1)
+        var register = findViewById<TextView>(R.id.signup)
 
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-            .requestIdToken(R.string.default_web)
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-
-        auth = FirebaseAuth.getInstance()
-        checkUser()
-
-        login_google.setOnClickListener {
-            Log.d(TAG,"Oncreate: begin Gogle SignIn")
-            val intent = googleSignInClient.signInIntent
-            startActivityForResult(intent, RC_SIGN_IN)
-        }
-
-        private fun FirebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
-            Log.d(TAG, "begin firebase with google account")
-
-            val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
-            FirebaseAuth.SignInWithCredential(credential)
-        }
-
-        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            super.onActivityResult(requestCode, resultCode, data)
-
-            if (requestCode == RC_SIGN_IN) {
-                Log.d(TAG, "onActivityResult: google signin result")
-                val accounttask = GoogleSignIn.getSignedInAccountFromIntent(data)
-                try {
-                    val account = accounttask.getResult(ApiException::class.java)
-                    FirebaseAuthWithGoogleAccount(account)
-                }
-
-                catch (e: Exception){
-                    Log.d(TAG, "onActivityResult: ${e.message}")
-                }
-            }
-        }
 
         login1.setOnClickListener {
             val userinput = emailid.text.toString()
             val passinput = password.text.toString()
+
             checkcredential(userinput, passinput)
         }
 
@@ -101,14 +42,10 @@ class login : AppCompatActivity() {
         }
 
         val txt1 = findViewById(R.id.txt1) as TextView
-        txt1.setOnClickListener{
+        txt1.setOnClickListener {
             val intent = Intent(this, forgotpass::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun checkUser() {
-        TODO("Not yet implemented")
     }
 
     private fun checkcredential(userinput: String, passinput: String) {
@@ -116,7 +53,7 @@ class login : AppCompatActivity() {
             auth.signInWithEmailAndPassword(userinput, passinput).addOnCompleteListener {
                 if (it.isSuccessful) {
                     Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, Home::class.java))
+                    startActivity(Intent(this, bluespot_main::class.java))
                     finish()
                 } else {
                     Toast.makeText(this, "Wrong credentials", Toast.LENGTH_SHORT).show()
@@ -125,4 +62,5 @@ class login : AppCompatActivity() {
         }
     }
 }
+
 
