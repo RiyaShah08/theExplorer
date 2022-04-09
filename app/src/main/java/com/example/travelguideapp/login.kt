663fcceaef6411e92e7b2c855bfa92a6b2793aa3
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-04-09 15:19:31 +0530

    Login with Google

--- app/src/main/java/com/example/travelguideapp/login.kt
+++ app/src/main/java/com/example/travelguideapp/login.kt
@@ -3,9 +3,11 @@ package com.example.travelguideapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import com.google.android.gms.auth.api.credentials.IdToken
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -17,13 +19,20 @@ import com.google.firebase.auth.GoogleAuthProvider
 
 class login : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
     private val RC_SIGN_IN = 89
+    private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id_auth))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         var emailid = findViewById<TextInputEditText>(R.id.emailid)
         var password = findViewById<TextInputEditText>(R.id.password)
@@ -31,10 +40,10 @@ class login : AppCompatActivity() {
         var register = findViewById<TextView>(R.id.signup)
         auth = FirebaseAuth.getInstance()
 
+
         login1.setOnClickListener {
             val userinput = emailid.text.toString()
             val passinput = password.text.toString()
-
             checkcredential(userinput, passinput)
         }
 
@@ -44,19 +53,11 @@ class login : AppCompatActivity() {
         }
 
         val txt1 = findViewById(R.id.txt1) as TextView
-        txt1.setOnClickListener {
+        txt1.setOnClickListener{
             val intent = Intent(this, forgotpass::class.java)
             startActivity(intent)
         }
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id_auth))
-            .requestEmail().build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
-
-
 
     private fun checkcredential(userinput: String, passinput: String) {
         if (userinput.isNotEmpty() && passinput.isNotEmpty()) {
@@ -74,8 +75,37 @@ class login : AppCompatActivity() {
 
     fun loginwithgoogle(view: android.view.View) {
         val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }catch (e: ApiException){
+                Log.i( "Error", "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful)
+                {
+                    Log.i("firebase", "signInWithCredential: success")
+                    val user = auth.currentUser
+
+                    startActivity(Intent(this, Home::class.java))
+                    Log.i("firebase", "firebaseAuthWithGoogle: ${user?.displayName}")
+                }else{
+                    Log.i("firebase", "signInWithCredential: failure", task.exception)
+                }
+            }
     }
 }
-
-
