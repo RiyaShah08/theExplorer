6ae2eaa7b38e281f712111177d8036cf1bf554bf
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-04-18 20:55:11 +0530

    In progress

--- app/src/main/java/com/example/travelguideapp/map_activity.kt
+++ app/src/main/java/com/example/travelguideapp/map_activity.kt
@@ -2,10 +2,29 @@ package com.example.travelguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class map_activity : AppCompatActivity() {
+class map_activity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+//        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+
+    }
+//
+    override fun onMapReady(googleMap: GoogleMap) {
+    val sydney = LatLng(-33.852, 151.211)
+    googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney")
+    )
     }
 }
