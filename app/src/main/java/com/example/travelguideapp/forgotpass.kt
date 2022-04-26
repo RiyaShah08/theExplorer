9cfdef6b60a07b9860a5f71e7759310d984eebea
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-04-26 23:34:54 +0530

    Reset Password with firebase

--- app/src/main/java/com/example/travelguideapp/forgotpass.kt
+++ app/src/main/java/com/example/travelguideapp/forgotpass.kt
@@ -2,10 +2,39 @@ package com.example.travelguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class forgotpass : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgotpass)
+
+        val button = findViewById(R.id.button) as TextView
+        val emailreset = findViewById(R.id.emailreset) as TextInputEditText
+
+        button.setOnClickListener{
+            val email: String = emailreset.text.toString().trim { it <= ' ' }
+            if(email.isEmpty()){
+                Toast.makeText(this@forgotpass, "Please enter your email address", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener{task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this@forgotpass,"Email sent successfully to reset your password!", Toast.LENGTH_LONG).show()
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this@forgotpass, task.exception!!.message.toString(),
+                            Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/travelguideapp/recycle_blue.kt
+++ app/src/main/java/com/example/travelguideapp/recycle_blue.kt
@@ -2,15 +2,79 @@ package com.example.travelguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.annotation.NonNull;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
+import android.widget.Toast
+import android.widget.TextView
+
 
 class recycle_blue : AppCompatActivity() {
+    // creating a variable for
+    // our Firebase Database.
+    var firebaseDatabase: FirebaseDatabase? = null
+
+    // creating a variable for our
+    // Database Reference for Firebase.
+    var databaseReference: DatabaseReference? = null
+
+    // variable for Text view.
+    private var retrieveTV: TextView? = null
+    private var retrieveTV1: TextView? = null
+    private var retrieveTV2: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycle_blue)
 
+        // below line is used to get the instance
+        // of our Firebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance()
 
+        // below line is used to get
+        // reference for our database.
+        databaseReference = firebaseDatabase!!.getReference("user1")
+
+        // initializing our object class variable.
+
+        retrieveTV2 = findViewById(R.id.imge1)
+        retrieveTV = findViewById(R.id.textView)
+        retrieveTV1 = findViewById(R.id.textView3)
+
+        // calling method
+        // for getting data.
+        getdata()
+    }
+
+    private fun getdata() {
+
+        // calling add value event listener method
+        // for getting the values from database.
+        databaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                val value = snapshot.getValue(String::class.java)
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                retrieveTV!!.text = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@recycle_blue, "Required Field must be not empty", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
