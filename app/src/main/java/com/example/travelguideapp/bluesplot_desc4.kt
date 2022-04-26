b36c3867fea30f599e5beccd3d726b9c35167394
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-04-27 00:09:37 +0530

    Upto Map activity

--- app/src/main/java/com/example/travelguideapp/bluesplot_desc4.kt
+++ app/src/main/java/com/example/travelguideapp/bluesplot_desc4.kt
@@ -1,11 +1,20 @@
 package com.example.travelguideapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class bluesplot_desc4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluesplot_desc4)
+
+        val txt5 = findViewById(R.id.back5) as ImageView
+        txt5.setOnClickListener {
+            val intent = Intent(this, bluespot_main::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/travelguideapp/bluespot_desc.kt
+++ app/src/main/java/com/example/travelguideapp/bluespot_desc.kt
@@ -3,6 +3,7 @@ package com.example.travelguideapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.google.android.gms.maps.GoogleMap
@@ -17,8 +18,15 @@ class bluespot_desc : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluespot_desc)
 
-        val txt1 = findViewById(R.id.bs1) as TextView
+        val txt1 = findViewById(R.id.back1) as ImageView
         txt1.setOnClickListener {
+            val intent = Intent(this, bluespot_main::class.java)
+            startActivity(intent)
+        }
+
+
+        val blue1 = findViewById(R.id.bs1) as TextView
+        blue1.setOnClickListener {
             val intent = Intent(this, map_activity::class.java)
             startActivity(intent)
         }
