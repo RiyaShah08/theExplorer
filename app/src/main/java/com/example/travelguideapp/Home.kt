18c74d628df00c07d1e19476322efd009d155eb1
Riya Shah <67892126+RiyaShah08@users.noreply.github.com>
2022-05-01 00:11:20 +0530

    onclicks, places, contactus done

--- app/src/main/java/com/example/travelguideapp/Home.kt
+++ app/src/main/java/com/example/travelguideapp/Home.kt
@@ -1,17 +1,36 @@
 package com.example.travelguideapp
 
+import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.HorizontalScrollView
 import android.widget.ImageView
 import android.widget.ScrollView
 import android.widget.TextView
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
 
 class Home : AppCompatActivity() {
+
+    val permission = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    val permissioncode = 78
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        askPermission();
+
+//        val isAlertWindowPermissionGranted: Boolean = TedPermissionUtil.isGranted(Manifest.permission.SYSTEM_ALERT_WINDOW)
+//        Log.d("ted", "isAlertWindowPermissionGranted: $isAlertWindowPermissionGranted")
+//
+//        getLocation();
 
         val card = findViewById(R.id.card) as ImageView
         card.setOnClickListener {
@@ -43,4 +62,21 @@ class Home : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun askPermission() {
+        ActivityCompat.requestPermissions(this, permission, permissioncode)
+    }
+
+//    private fun getLocation()
+//    {
+//        val permissionlistener: PermissionListener = object : PermissionListener() {
+//            fun onPermissionGranted() {
+//                Toast.makeText(this@Home, "Permission Granted", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            fun onPermissionDenied(deniedPermissions: List<String?>) {
+//                Toast.makeText(this@Home, "Permission Denied\n$deniedPermissions", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/example/travelguideapp/discover_des1.kt
+++ app/src/main/java/com/example/travelguideapp/discover_des1.kt
@@ -3,7 +3,9 @@ package com.example.travelguideapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 
 class discover_des1 : AppCompatActivity() {
@@ -17,5 +19,26 @@ class discover_des1 : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val backbut = findViewById(R.id.back4) as ImageView
+        backbut.setOnClickListener {
+            val intent = Intent(this, discover_beauty::class.java)
+            startActivity(intent)
+        }
+
+        var bookmark = findViewById(R.id.bookmark3) as CheckBox
+        bookmark.setOnCheckedChangeListener{ checkBox, isChecked->
+
+            if(isChecked){
+                showToast("Item added to wishlist");
+            }
+            else{
+                showToast("Item removed from wishlist");
+            }
+        }
+
+    }
+
+    private fun showToast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
     }
 }
